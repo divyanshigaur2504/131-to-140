c84f96a197b77a3f33a8189088ec7ace116a69de
divyanshigaur2504 <divyanshigaur108@gmail.com>
2025-12-06 16:00:53 +0530

    Create 138.c

---
+++ 138.c
@@ -0,0 +1,27 @@
+/*Q138: Print all enum names and integer values using a loop.
+
+/*
+Sample Test Cases:
+Input 1:
+No input
+Output 1:
+RED=0
+YELLOW=1
+GREEN=2
+
+*/
+#include <stdio.h>
+
+enum TrafficLight { RED, YELLOW, GREEN };
+
+int main() {
+    
+    char *names[] = { "RED", "YELLOW", "GREEN" };
+
+    
+    for (int i = RED; i <= GREEN; i++){
+        printf("%s=%d\n", names[i], i);
+    }
+
+    return 0;
+}
