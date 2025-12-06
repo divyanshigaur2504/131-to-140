6e75c5028c39a1aadf7aa9a7b284690613eabd78
divyanshigaur2504 <divyanshigaur108@gmail.com>
2025-12-06 15:49:07 +0530

    Create 132.c

---
+++ 132.c
@@ -0,0 +1,34 @@
+/*Q131: Create an enumeration for days (SUNDAY to SATURDAY) and print each day with its integer value.
+
+/*
+Sample Test Cases:
+Input 1:
+No input
+Output 1:
+SUNDAY = 0
+MONDAY = 1
+TUESDAY = 2
+WEDNESDAY = 3
+THURSDAY = 4
+FRIDAY = 5
+SATURDAY = 6
+
+*/
+#include <stdio.h>
+
+enum Days { SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY };
+
+int main() {
+  
+    char *dayNames[] = {
+        "SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY",
+        "THURSDAY", "FRIDAY", "SATURDAY"
+    };
+
+  
+    for (int d = SUNDAY; d <= SATURDAY; d++) {
+        printf("%s = %d\n", dayNames[d], d);
+    }
+
+    return 0;
+}
