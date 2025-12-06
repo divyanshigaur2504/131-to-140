7cc7f3528ea64f00d6772b93d2664a60e7df186e
divyanshigaur2504 <divyanshigaur108@gmail.com>
2025-12-06 15:55:08 +0530

    Create 135.c

---
+++ 135.c
@@ -0,0 +1,27 @@
+/*Q135: Assign explicit values starting from 10 and print them.
+
+/*
+Sample Test Cases:
+Input 1:
+No input
+Output 1:
+A=10
+B=11
+C=12
+
+*/
+#include <stdio.h>
+
+
+enum Numbers { A = 10, B, C, D, E };
+
+int main() {
+    
+    printf("A = %d\n", A);
+    printf("B = %d\n", B);
+    printf("C = %d\n", C);
+    printf("D = %d\n", D);
+    printf("E = %d\n", E);
+
+    return 0;
+}
